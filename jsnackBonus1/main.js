6da7866c1165ac7a46d987020173df4a1bf43a41
LorenzoPrimogeri <96630976+LorenzoPrimogeri@users.noreply.github.com>
2022-03-22 15:51:43 +0100

    complete

--- jsnackBonus1/main.js
+++ jsnackBonus1/main.js
@@ -1,10 +1,14 @@
 const array1 = [1, 2, 3, 4, 5, 6, 7];
 const array2 = ["a", "b", "c", "d", "e", "f", "g"];
-const unione = [];
 
-for (let i = 0; i < array1.length; i++) {
-    const element1 = array1[i];
-    const element2 = array2[i];
-    unione.push(element1);
-    unione.push(element2);
-} console.log(unione);
+function unisciArray() {
+    const unione = [];
+    for (let i = 0; i < array1.length; i++) {
+        const element1 = array1[i];
+        const element2 = array2[i];
+        unione.push(element1);
+        unione.push(element2);
+    } return unione;
+}
+const unione = unisciArray();
+console.log(unione);
